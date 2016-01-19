af14b5553a5b72006c5351b48fba40f54dc0a3cf
dudley405 <dudley405@gmail.com>
2016-01-18 22:23:11 -0600

    refactoring

--- app/src/main/java/com/dudley/towerdefense/sprite/Sprite.java
+++ app/src/main/java/com/dudley/towerdefense/sprite/Sprite.java
@@ -1,4 +1,4 @@
-package com.dudley.towerdefense.sprite.enemy;
+package com.dudley.towerdefense.sprite;
 
 import android.graphics.Bitmap;
 import android.graphics.Paint;
@@ -14,109 +14,38 @@ import java.util.Random;
 /**
  * Created by Justin on 1/3/2016.
  */
-public class EnemySprite {
+public class Sprite {
 
     protected int BMP_ROWS = 8;
     protected int BMP_COLUMNS = 12;
-    protected int x = 0;
-    protected int y = 0;
-    protected int[] mX;
-    protected int[] mY;
-    protected int lastXPoint;
-    protected int lastYPoint;
     protected Graphics graphics;
     protected Bitmap bmp;
+    public int x;
+    public int y;
+    protected int[] mX;
+    protected int[] mY;
+    public int lastXPoint;
+    public int lastYPoint;
     protected int width;
     protected int height;
-    private int speed = 2;
     public int health;
+    Path path;
+    PathMeasure pathMeasure = new PathMeasure();
+    int speed;
 
-    boolean isMovingRight;
-    boolean isMovingLeft;
-    boolean isMovingDown;
-    boolean isMovingUp;
-
-    boolean isFinished = false;
+    public boolean isFinished = false;
 
     public int i = 0;
 
-    Animation animationLeft = new Animation();
-    Animation animationRight = new Animation();
-    Animation animationUp = new Animation();
-    Animation animationDown = new Animation();
-
-    Path path;
-    PathMeasure pathMeasure = new PathMeasure();
-
     Paint paint;
 
-    public EnemySprite(Graphics graphics, Bitmap bmp) {
+    public Sprite(Graphics graphics, Bitmap bmp) {
         this.graphics = graphics;
         this.bmp = bmp;
         this.width = bmp.getWidth() / BMP_COLUMNS;
         this.height = bmp.getHeight() / BMP_ROWS;
 
         paint = new Paint(Paint.FILTER_BITMAP_FLAG);
-    }
-
-    protected void update() {
-        getPathPoints();
-
-        // Set animation based on direction of movement
-        if (x < lastXPoint) {
-            moveLeft();
-        } else if (x > lastXPoint) {
-            moveRight();
-        } else if (y < lastYPoint) {
-            moveUp();
-        } else if (y > lastYPoint) {
-            moveDown();
-        }
-    }
-
-    public void onDraw() {};
-
-    /*
-     * Here we want to distinguish which direction the sprite is moving
-     * so that we can display the appropriate animation (bunny is moving right
-     * animation, bunny is moving left animiation, etc.)
-     */
-
-    public void moveRight() {
-        isMovingRight = true;
-        isMovingLeft = false;
-        isMovingUp = false;
-        isMovingDown = false;
-    }
-
-
-    public void moveLeft() {
-        isMovingLeft = true;
-        isMovingRight = false;
-        isMovingUp = false;
-        isMovingDown = false;
-    }
-
-    public void moveUp() {
-        isMovingUp = true;
-        isMovingLeft = false;
-        isMovingRight = false;
-        isMovingDown = false;
-    }
-
-    public void moveDown() {
-        isMovingDown = true;
-        isMovingLeft = false;
-        isMovingRight = false;
-        isMovingUp = false;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public int getSpeed() {
-        return this.speed;
     }
 
     /**
@@ -140,8 +69,12 @@ public class EnemySprite {
         }
     }
 
-    protected Path getPath() {
-        return this.path;
+    public void onDraw() {
+        update();
+    }
+
+    public void update() {
+
     }
 
     /**
@@ -180,12 +113,21 @@ public class EnemySprite {
         }
     }
 
+    public boolean isFinished() {
+        return isFinished;
+    }
+
     public int getXCoord() {
         return x;
     }
 
     public int getYCoord() {
         return y;
+    }
+
+
+    public void hit(int damage) {
+        health -= damage;
     }
 
     public int getWidth() {
@@ -196,15 +138,8 @@ public class EnemySprite {
         return height;
     }
 
-    public boolean isFinished() {
-        return isFinished;
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 
-    public void setFinished() {
-        this.isFinished = true;
-    }
-
-    public void hit(int damage) {
-        health -= damage;
-    }
 }
